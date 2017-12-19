8c3c787660e2a18a54753e8eec141037c6d60049
radjaiqbal99 <sanusiiqbal36@gmail.com>
2017-12-19 11:54:07 +0700

    initial commit

---
+++ app/src/main/java/com/example/radjaiqbalsh/radjaiqbal/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.asus.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ASUS on 19-Dec-17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_form);
+    }
+}
